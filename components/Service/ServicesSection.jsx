19e6a183c19b0ff07fbdc4b0b6b5bfef92c24eb1
sanjat <sanjayandani789@gmail.com>
2025-04-22 11:39:15 -0700

    service page update

--- components/Service/ServicesSection.jsx
+++ components/Service/ServicesSection.jsx
@@ -2,133 +2,122 @@
 import Link from "next/link";
 
 export default function ServicesSection() {
-    return (
-        // <!-- SERVICES SECTION START -->
-        <section className="ul-inner-services ul-section-spacing overflow-hidden">
-            <div className="ul-inner-services-container">
-                <div className="row row-cols-md-3 row-cols-2 row-cols-xxs-1 ul-bs-row">
-                    {/* <!-- single slide --> */}
-                    <div className="col wow animate__fadeInUp">
-                        <div className="ul-inner-service">
-                            <div className="ul-inner-service-txt">
-                                <div className="ul-inner-service-icon">
-                                    <i className="flaticon-settings-1"></i>
-                                </div>
-                                <div className="ul-inner-service-content">
-                                    <h3 className="ul-inner-service-title"><Link href="/servicedetail">Digital Marketing</Link></h3>
-                                    <p className="ul-inner-service-descr">Lorem dolor amet consectetur read adipiscing any more elit.</p>
-                                    <Link href="/servicedetail" className="ul-inner-service-link">Read More <i className="flaticon-next-1"></i></Link>
-                                </div>
-                            </div>
-
-                            <div className="ul-inner-service-img">
-                                <img src="webImages/inner-service-1.jpg" alt="Image" className=""/>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* <!-- single slide --> */}
-                    <div className="col wow animate__fadeInUp">
-                        <div className="ul-inner-service">
-                            <div className="ul-inner-service-txt">
-                                <div className="ul-inner-service-icon">
-                                    <i className="flaticon-seo"></i>
-                                </div>
-                                <div className="ul-inner-service-content">
-                                    <h3 className="ul-inner-service-title"><Link href="/servicedetail">SEO Analytics</Link></h3>
-                                    <p className="ul-inner-service-descr">Lorem dolor amet consectetur read adipiscing any more elit.</p>
-                                    <Link href="/servicedetail" className="ul-inner-service-link">Read More <i className="flaticon-next-1"></i></Link>
-                                </div>
-                            </div>
-
-                            <div className="ul-inner-service-img">
-                                <img src="webImages/inner-service-2.jpg" alt="Image" className=""/>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* <!-- single slide --> */}
-                    <div className="col wow animate__fadeInUp">
-                        <div className="ul-inner-service">
-                            <div className="ul-inner-service-txt">
-                                <div className="ul-inner-service-icon">
-                                    <i className="flaticon-content-marketing"></i>
-                                </div>
-                                <div className="ul-inner-service-content">
-                                    <h3 className="ul-inner-service-title"><Link href="/servicedetail">Social Marketing</Link></h3>
-                                    <p className="ul-inner-service-descr">Lorem dolor amet consectetur read adipiscing any more elit.</p>
-                                    <Link href="/servicedetail" className="ul-inner-service-link">Read More <i className="flaticon-next-1"></i></Link>
-                                </div>
-                            </div>
-
-                            <div className="ul-inner-service-img">
-                                <img src="webImages/inner-service-3.jpg" alt="Image" className="" />
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* <!-- single slide --> */}
-                    <div className="col wow animate__fadeInUp">
-                        <div className="ul-inner-service">
-                            <div className="ul-inner-service-txt">
-                                <div className="ul-inner-service-icon">
-                                    <i className="flaticon-email-1"></i>
-                                </div>
-                                <div className="ul-inner-service-content">
-                                    <h3 className="ul-inner-service-title"><Link href="/servicedetail">Email Marketing</Link></h3>
-                                    <p className="ul-inner-service-descr">Lorem dolor amet consectetur read adipiscing any more elit.</p>
-                                    <Link href="/servicedetail" className="ul-inner-service-link">Read More <i className="flaticon-next-1"></i></Link>
-                                </div>
-                            </div>
-
-                            <div className="ul-inner-service-img">
-                                <img src="webImages/inner-service-4.jpg" alt="Image" className="" />
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* <!-- single slide --> */}
-                    <div className="col wow animate__fadeInUp">
-                        <div className="ul-inner-service">
-                            <div className="ul-inner-service-txt">
-                                <div className="ul-inner-service-icon">
-                                    <i className="flaticon-coding"></i>
-                                </div>
-                                <div className="ul-inner-service-content">
-                                    <h3 className="ul-inner-service-title"><Link href="/servicedetail">Web Development</Link></h3>
-                                    <p className="ul-inner-service-descr">Lorem dolor amet consectetur read adipiscing any more elit.</p>
-                                    <Link href="/servicedetail" className="ul-inner-service-link">Read More <i className="flaticon-next-1"></i></Link>
-                                </div>
-                            </div>
-
-                            <div className="ul-inner-service-img">
-                                <img src="webImages/inner-service-5.jpg" alt="Image" className="" />
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* <!-- single slide --> */}
-                    <div className="col wow animate__fadeInUp">
-                        <div className="ul-inner-service">
-                            <div className="ul-inner-service-txt">
-                                <div className="ul-inner-service-icon">
-                                    <i className="flaticon-web-design"></i>
-                                </div>
-                                <div className="ul-inner-service-content">
-                                    <h3 className="ul-inner-service-title"><Link href="/servicedetail">UI/UX Design</Link></h3>
-                                    <p className="ul-inner-service-descr">Lorem dolor amet consectetur read adipiscing any more elit.</p>
-                                    <Link href="/servicedetail" className="ul-inner-service-link">Read More <i className="flaticon-next-1"></i></Link>
-                                </div>
-                            </div>
-
-                            <div className="ul-inner-service-img">
-                                <img src="webImages/inner-service-6.jpg" alt="Image" className="" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="ul-section-spacing">
+      <div className="ul-case-study-container">
+        <div className="row ul-bs-row row-cols-lg-3 row-cols-sm-2 row-cols-1">
+          {/* <!-- single case study --> */}
+          <div className="col">
+            <div className="ul-case-study-item ul-inner-case-study-item">
+              <div className="img">
+                <img src="webImages/case-study-1.jpg" alt="Case Study Image" />
+              </div>
+              <div className="txt">
+                <span className="ul-case-study-item-category">Mobile App</span>
+                <h3 className="ul-case-study-item-title">Digital Banking App</h3>
+              </div>
+              <div className="ul-case-study-item-bottom">
+                <span className="ul-case-study-item-index">01</span>
+                <Link href="/" className="ul-case-study-item-btn">
+                  <i className="flaticon-next-1"></i>
+                </Link>
+              </div>
             </div>
-        </section>
-        // {/* <!-- SERVICES SECTION END --> */}
-    );
+          </div>
+          {/* <!-- single case study --> */}
+          <div className="col">
+            <div className="ul-case-study-item ul-inner-case-study-item">
+              <div className="img">
+                <img src="webImages/case-study-2.jpg" alt="Case Study Image" />
+              </div>
+              <div className="txt">
+                <span className="ul-case-study-item-category">Web App</span>
+                <h3 className="ul-case-study-item-title">E-commerce Website</h3>
+              </div>
+              <div className="ul-case-study-item-bottom">
+                <span className="ul-case-study-item-index">02</span>
+                <Link href="/" className="ul-case-study-item-btn">
+                  <i className="flaticon-next-1"></i>
+                </Link>
+              </div>
+            </div>
+          </div>
+          {/* <!-- single case study --> */}
+          <div className="col">
+            <div className="ul-case-study-item ul-inner-case-study-item">
+              <div className="img">
+                <img src="webImages/case-study-3.jpg" alt="Case Study Image" />
+              </div>
+              <div className="txt">
+                <span className="ul-case-study-item-category">Technology</span>
+                <h3 className="ul-case-study-item-title">Smart Home Automation</h3>
+              </div>
+              <div className="ul-case-study-item-bottom">
+                <span className="ul-case-study-item-index">03</span>
+                <Link href="/" className="ul-case-study-item-btn">
+                  <i className="flaticon-next-1"></i>
+                </Link>
+              </div>
+            </div>
+          </div>
+          {/* <!-- single case study --> */}
+          <div className="col">
+            <div className="ul-case-study-item ul-inner-case-study-item">
+              <div className="img">
+                <img src="webImages/case-study-2.jpg" alt="Case Study Image" />
+              </div>
+              <div className="txt">
+                <span className="ul-case-study-item-category">Mobile App</span>
+                <h3 className="ul-case-study-item-title">Healthcare App</h3>
+              </div>
+              <div className="ul-case-study-item-bottom">
+                <span className="ul-case-study-item-index">04</span>
+                <Link href="/" className="ul-case-study-item-btn">
+                  <i className="flaticon-next-1"></i>
+                </Link>
+              </div>
+            </div>
+          </div>
+          {/* <!-- single case study --> */}
+          <div className="col">
+            <div className="ul-case-study-item ul-inner-case-study-item">
+              <div className="img">
+                <img src="webImages/case-study-3.jpg" alt="Case Study Image" />
+              </div>
+              <div className="txt">
+                <span className="ul-case-study-item-category">Web App</span>
+                <h3 className="ul-case-study-item-title">ERM Software</h3>
+              </div>
+              <div className="ul-case-study-item-bottom">
+                <span className="ul-case-study-item-index">05</span>
+                <Link href="/" className="ul-case-study-item-btn">
+                  <i className="flaticon-next-1"></i>
+                </Link>
+              </div>
+            </div>
+          </div>
+          {/* <!-- single case study --> */}
+          <div className="col">
+            <div className="ul-case-study-item ul-inner-case-study-item">
+              <div className="img">
+                <img src="webImages/case-study-1.jpg" alt="Case Study Image" />
+              </div>
+              <div className="txt">
+                <span className="ul-case-study-item-category">Technology</span>
+                <h3 className="ul-case-study-item-title">
+                  Digital Banking Platform
+                </h3>
+              </div>
+              <div className="ul-case-study-item-bottom">
+                <span className="ul-case-study-item-index">06</span>
+                <Link href="/" className="ul-case-study-item-btn">
+                  <i className="flaticon-next-1"></i>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }

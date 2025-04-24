a0a360ed59dd77657cdd091e742822b701304e7a
Muhammad Saim Raza <msraza.02@gmail.com>
2025-04-24 23:42:13 +0500

    Update ServicesSection.jsx

--- components/Service/ServicesSection.jsx
+++ components/Service/ServicesSection.jsx
@@ -13,7 +13,7 @@ export default function ServicesSection() {
       <div className="ul-case-study-container">
         <div className="row ul-bs-row row-cols-lg-3 row-cols-sm-2 row-cols-1 g-5">
           {/* <!-- single service --> */}
-          {mainServices.map((service) => (
+          {mainServices.map((service,key) => (
             <div className="col" key={service.id}>
               <div className="ul-case-study-item ul-inner-case-study-item">
                 <div className="img">
@@ -26,7 +26,7 @@ export default function ServicesSection() {
                   <h3 className="ul-case-study-item-title">{service.title}</h3>
                 </div>
                 <div className="ul-case-study-item-bottom">
-                  <span className="ul-case-study-item-index">{service.id}</span>
+                  <span className="ul-case-study-item-index">{String(key + 1).padStart(2, '0')}</span>
                   <Link
                     href={`/services/${service.slug}`}
                     className="ul-case-study-item-btn"

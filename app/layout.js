1dc00634f2a66814cf9f025b5db66c3030d5293f
Muhammad Saim Raza <msraza.02@gmail.com>
2025-04-30 23:31:46 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ import Preloader from "@/components/Preloader";
 import Sidebar from "@/components/Sidebar";
 import Search from "@/components/Search";
 import { Toaster } from 'react-hot-toast';
+import Script from "next/script";
 
 export const metadata = {
   title: "Innovating the Future of Tech | ITnnovator",
@@ -69,8 +70,27 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="stylesheet" href="/assets/css/flaticon.css" />
+        {/* Google Tag Manager */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M6M4F75H');
+          `}
+        </Script>
       </head>
       <body className={`${plusJakarta.variable} ${kumbh.variable} ${quicksand.variable}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M6M4F75H"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         {/* Toast container */}
         <Toaster position="top-right" reverseOrder={false} />
         <BootstrapClient />
